471f1b696546bca6b10a011ac4b59935372a0a47
Eric Schles <ericschles@gmail.com>
2017-01-13 16:32:58 -0500

    fixed a small bug

--- app/views.py
+++ app/views.py
@@ -1,34 +1,67 @@
 from app import app, db, viz
-from flask import request, render_template,redirect, url_for
+from flask import request, render_template,redirect, url_for, Response
 import json
-from app.models import Jokes
+from app.models import Jokes, Users
 from datetime import datetime
 from glob import glob
 import os
 import random
+from functools import wraps
+
+
+def check_auth(username, password):
+    """This function is called to check if a username /
+    password combination is valid.
+    """
+    result = Users.query.filter_by(username=username).all()
+    if len(result) == 0:
+        return False
+    elif result[0].password != password:
+        return False
+    else:
+        return True
+
+    
+def authenticate():
+    """Sends a 401 response that enables basic auth"""
+    return Response(
+    'Could not verify your access level for that URL.\n'
+    'You have to login with proper credentials', 401,
+    {'WWW-Authenticate': 'Basic realm="Login Required"'})
+
+
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        auth = request.authorization
+        if not auth or not check_auth(auth.username, auth.password):
+            return authenticate()
+        return f(*args, **kwargs)
+    return decorated
+
 
 def increment_value(obj,decision):
-    if decision == "dad_joke":
+    if decision == "dad_jokes":
         obj.dad_joke += 1
         listing = json.loads(obj.dad_timestamps)
         listing.append(str(datetime.now()))
         obj.dad_timestamps = json.dumps(listing)
-    elif decision == "nerd_joke":
+    elif decision == "nerd_jokes":
         obj.nerd_joke += 1
         listing = json.loads(obj.nerd_timestamps)
         listing.append(str(datetime.now()))
         obj.nerd_timestamps = json.dumps(listing)
-    elif decision == "weird_joke":
+    elif decision == "weird_jokes":
         obj.weird_joke += 1
         listing = json.loads(obj.weird_timestamps)
         listing.append(str(datetime.now()))
         obj.weird_timestamps = json.dumps(listing)
-    elif decision == "cat_meme":
+    elif decision == "cat_memes":
         obj.cat_meme += 1
         listing = json.loads(obj.cat_timestamps)
         listing.append(str(datetime.now()))
         obj.cat_timestamps = json.dumps(listing)
-    elif decision == "dog_meme":
+    elif decision == "dog_memes":
         obj.dog_meme += 1
         listing = json.loads(obj.dog_timestamps)
         listing.append(str(datetime.now()))
@@ -36,7 +69,8 @@ def increment_value(obj,decision):
     return obj
 
 
-@app.route("/",methods=["GET","POST"])
+@app.route("/index",methods=["GET","POST"])
+@requires_auth
 def index():
     starting_directory = os.getcwd()
     if not starting_directory.endswith("img"):
@@ -57,13 +91,15 @@ def index():
 
 @app.route("/joke_decision", methods=["GET","POST"])
 def joke_decision():
-    user_id = request.form.get("user_id") #this will pull in the information from the cookie with an implicit field from the form
+    auth = request.authorization
     
+    user_id = auth.username #this will pull in the information from the cookie with an implicit field from the form
     if request.form.get("image_one"):
         decision = request.form.get("image_one")
     else:
         decision = request.form.get("image_two")
-    joke = Jokes.query.filter_by(user_id=user_id)
+    
+    joke = Jokes.query.filter_by(user_id=user_id).first()
     joke = increment_value(joke,decision)
     db.session.add(joke)
     db.session.commit()
@@ -89,13 +125,17 @@ def sign_up():
             user = Users(username=username,password=password)
             db.session.add(user)
             db.session.commit()
-            print(username,password)
+            joke = Jokes(0, 0, 0, 0, 0,
+                         json.dumps([]), json.dumps([]), json.dumps([]), json.dumps([]), json.dumps([]),
+                         username)
+            db.session.add(joke)
+            db.session.commit()
             return redirect(url_for("sign_in"))
         else:
             return render_template("sign_up.html",error="username already exists, please choose another")
     return render_template("sign_up.html")
 
-@app.route("/sign-in",methods=["GET","POST"])
+@app.route("/",methods=["GET","POST"])
 def sign_in():
     """
     Takes in username and password and checks to see in the database
@@ -108,7 +148,6 @@ def sign_in():
     """
     if request.method=="POST":
         
-        print(type(request.form))
         username = request.form.get("username")
         password = request.form.get("password_field")
         result = Users.query.filter_by(username=username).all()
@@ -117,7 +156,7 @@ def sign_in():
         elif result[0].password != password:
             return render_template("sign_in.html",error="wrong password")
         else:
-            return redirect(url_for("review_an_article"))
+            return redirect(url_for("index"))
     return render_template("sign_in.html")
 
 
