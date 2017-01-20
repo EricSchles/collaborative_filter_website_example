d8e0dee98a0d8d9c7233b1fabd9d13782ac7b3fe
Eric Schles <ericschles@gmail.com>
2017-01-20 17:42:55 -0500

    added some more stuff to the data modeling

--- app/tools.py
+++ app/tools.py
@@ -3,45 +3,128 @@ from pgmpy.models import BayesianModel
 from pgmpy.factors import TabularCPD
 from app.models import Jokes
 
-number_of_users = len(set([joke.user_id for joke in Jokes.query.all()]))
-# Defining the model structure. We can define the network by just passing a list of edges.
-model = BayesianModel([('', '')])
+def main():
+    number_of_users = len(set([joke.user_id for joke in Jokes.query.all()]))
+    # Defining the model structure. We can define the network by just passing a list of edges.
+    model = BayesianModel([('Decision', 'User')])
 
-# Defining individual CPDs.
-cpd_decision = TabularCPD(variable='Decision', variable_card=5, values=[]) #number of times this user has chosen a category
-cpd_preference = TabularCPD(variable='Preference', variable_card=10, values=[]) #number of times they've chosen a category given another option
-cpd_user_category = TabularCPD(variable='UserCategory', variable_card=5, values=[]) #number of overall users on the site with each category as a preference
-cpd_follower = TabularCPD(variable='Follower', variable_card=2, values=[]) #they follow other people or they don't
+    # Defining individual CPDs.
 
-# The representation of CPD in pgmpy is a bit different than the CPD shown in the above picture. In pgmpy the colums
-# are the evidences and rows are the states of the variable. So the grade CPD is represented like this:
-#
-#    +---------+---------+---------+---------+---------+
-#    | diff    | intel_0 | intel_0 | intel_1 | intel_1 |
-#    +---------+---------+---------+---------+---------+
-#    | intel   | diff_0  | diff_1  | diff_0  | diff_1  |
-#    +---------+---------+---------+---------+---------+
-#    | grade_0 | 0.3     | 0.05    | 0.9     | 0.5     |
-#    +---------+---------+---------+---------+---------+
-#    | grade_1 | 0.4     | 0.25    | 0.08    | 0.3     |
-#    +---------+---------+---------+---------+---------+
-#    | grade_2 | 0.3     | 0.7     | 0.02    | 0.2     |
-#    +---------+---------+---------+---------+---------+
+    jokes = Jokes.query.all()
+    total_jokes = sum([joke.nerd_joke + joke.weird_joke + joke.cat_meme + joke.dog_meme + joke.dad_joke
+                   for joke in jokes])
 
-cpd_user = TabularCPD(variable='User', variable_card=5, 
-                   values=[],
-                  evidence=['Decision', 'Preference'],
-                  evidence_card=[5, 10])
+    prob_nerd_jokes = sum([joke.nerd_joke for joke in jokes])/float(total_jokes)
+    prob_weird_jokes = sum([joke.weird_joke for joke in jokes])/float(total_jokes)
+    prob_cat_memes = sum([joke.cat_meme for joke in jokes])/float(total_jokes)
+    prob_dog_memes = sum([joke.dog_meme for joke in jokes])/float(total_jokes)
+    prob_dad_jokes = sum([joke.dad_joke for joke in jokes])/float(total_jokes)
+    cpd_decision = TabularCPD(variable='Decision', variable_card=5, values=[prob_nerd_jokes, prob_weird_jokes, prob_cat_memes, prob_dog_memes, prob_dad_jokes])
+    #number of times this user has chosen a category
 
-cpd_choice = TabularCPD(variable='Choice', variable_card=, 
-                   values=[],
-                   evidence=['Decision', 'Preferences','UserCategory','Follower'],
-                   evidence_card=[])
+    # num_dad_over_nerd = sum([joke.dad_over_nerd for joke in jokes])/float(total_jokes)
+    # num_nerd_over_dad = sum([joke.nerd_over_dad for joke in jokes])/float(total_jokes)
+    # num_dad_over_weird = sum([joke.dad_over_weird for joke in jokes])/float(total_jokes)
+    # num_weird_over_dad = sum([joke.weird_over_dad for joke in jokes])/float(total_jokes)
+    # num_dad_over_cat = sum([joke.dad_over_cat for joke in jokes])/float(total_jokes)
+    # num_cat_over_dad = sum([joke.cat_over_dad for joke in jokes])/float(total_jokes)
+    # num_dad_over_dog = sum([joke.dad_over_dog for joke in jokes])/float(total_jokes)
+    # num_dog_over_dad = sum([joke.dog_over_dad for joke in jokes])/float(total_jokes)
+    # num_nerd_over_weird = sum([joke.nerd_over_weird for joke in jokes])/float(total_jokes)
+    # num_weird_over_nerd = sum([joke.weird_over_nerd for joke in jokes])/float(total_jokes)
+
+    #cpd_preference = TabularCPD(variable='Preference', variable_card=20, values=[])
+    #number of times they've chosen a category given another option
 
 
-# Associating the CPDs with the network
-model.add_cpds() #ToDo
+    # The representation of CPD in pgmpy is a bit different than the CPD shown in the above picture. In pgmpy the colums
+    # are the evidences and rows are the states of the variable. So the grade CPD is represented like this:
+    #
+    #    +---------  +---------+---------+---------+---------+
+    #    | Decision  | dad     | cat     | dog     | weird   | nerd |
+    #    +---------  +---------+---------+---------+---------+
+    #    | user_dad | 0.3     | 0.05    | 0.9     | 0.5     |
+    #    +---------+---------+---------+---------+---------+
+    #    | user_cat | 0.4     | 0.25    | 0.08    | 0.3     |
+    #    +---------+---------+---------+---------+---------+
+    #    | user_nerd | 0.3     | 0.7     | 0.02    | 0.2     |
+    #    +---------+---------+---------+---------+---------+
+    #    | user_dog | 0.3     | 0.7     | 0.02    | 0.2     |
+    #    +---------+---------+---------+---------+---------+
+    #    | user_weird | 0.3     | 0.7     | 0.02    | 0.2     |
+    #    +---------+---------+---------+---------+---------+
 
-# check_model checks for the network structure and CPDs and verifies that the CPDs are correctly 
-# defined and sum to 1.
-model.check_model()
+    #number of overall users on the site with each category as a preference
+    # a user prefers a type of joke if the most jokes they clicked on, is of a given type
+    total_num_users = Joke.query.count()
+    user_type_counts = {"nerd":0,"weird":0,"dad":0,"cat":0,"dog":0}
+    user_preference_counts = {
+        "nerd":{
+            "weird":0,
+            "dad": 0,
+            "cat": 0,
+            "dog": 0,
+            "nerd": 0
+            },
+        "weird":{
+            "nerd": 0,
+            "dad": 0,
+            "cat": 0,
+            "dog": 0,
+            "weird": 0
+        },
+        "cat":{
+            "nerd":0,
+            "dad":0,
+            "dog":0,
+            "weird":0,
+            "cat":0
+        },
+        "dog":{
+            "nerd":0,
+            "dog":0,
+            "cat":0,
+            "weird":0,
+            "dad":0
+        },
+        "dad":{
+            "dad":0,
+            "nerd":0,
+            "weird":0,
+            "cat":0,
+            "dog":0
+        }
+    }
+
+    list_of_keys = ["nerd", "weird", "dad", "cat", "dog"]
+    for joke in jokes:
+        dicter = {
+            "nerd":joke.nerd_joke,
+            "weird":joke.weird_joke,
+            "dad":joke.dad_joke,
+            "cat":joke.cat_meme,
+            "dog":joke.dog_meme
+        }
+        for key in list_of_keys:
+            user_preference_counts[ user_type_counts[ max(dicter) ] ][key] = dicter[key]/float(total_jokes)
+        
+
+    # cpd_user = TabularCPD(variable='User', variable_card=5, 
+    #                    values=[ user_table_results ],
+    #                   evidence=['Decision'],
+    #                   evidence_card=[5])
+
+    # # cpd_follower = TabularCPD(variable='Follower', variable_card=2, values=[]) #they follow other people or they don't
+
+    # # cpd_choice = TabularCPD(variable='Choice', variable_card=, 
+    # #                    values=[],
+    # #                    evidence=['Decision', 'Preferences','UserCategory','Follower'],
+    # #                    evidence_card=[])
+
+
+    # # Associating the CPDs with the network
+    # model.add_cpds(cpd_decision, cpd_user) #ToDo
+
+    # # check_model checks for the network structure and CPDs and verifies that the CPDs are correctly 
+    # # defined and sum to 1.
+    # print(model.check_model())
